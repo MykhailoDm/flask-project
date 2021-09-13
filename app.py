e3285f6dfc1fd817e0443fff2e06bd69287e8460
MykhailoDm <mikhailo2002dm@gmail.com>
2021-09-14 00:20:32 +0300

    Change code appropriate to the task lab 4

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/hello-world-9')
+def hello_world():
+    return 'Hello World 9'
 
 
 if __name__ == '__main__':
